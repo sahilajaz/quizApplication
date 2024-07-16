1c702298a81bb2bf23afbcbf46f1cd4580c09f37
sahilajaz <sahilajazw@gmail.com>
2024-07-16 23:01:53 +0530

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import QuestionFrame from './components/QuestionFrame';
 import data from './data';
 
 function App() {
   const [clickedButtonIndices, setClickedButtonIndices] = useState({})
+  const [sizeOfObject, setSizeOfObject] = useState(0);
+
 
   const buttonColorHandler = (questionIndex, buttonIndex) => {
     setClickedButtonIndices((prevState) => ({
       ...prevState,
       [questionIndex]: buttonIndex,
     }))
-  }
+  } 
+  
  
 
   const getButtonStyle = (questionIndex, buttonIndex) => ({
-    backgroundColor: clickedButtonIndices.questionIndex === buttonIndex ? 'rgb(147, 176, 186)' : 'white',
+    backgroundColor: clickedButtonIndices[questionIndex] === buttonIndex ? 'rgb(147, 176, 186)' : 'white',
   })
 
- 
+   useEffect(() => {
+       setSizeOfObject(Object.keys(clickedButtonIndices).length)
+   } , [clickedButtonIndices])
 
+   const totalLengthOfDataArray = data.results.length
+   
+
+
+  
+  
+ 
   const questionData = data.results.map((el, index) => (
     <QuestionFrame
       key={index}
@@ -31,11 +43,19 @@ function App() {
     />
   ));
 
+  const showAnswerButton = (
+     <button className="newquiz-btn">show answer</button>
+  )
+
+  const newQuizButton = (
+    <button className="newquiz-btn">new quiz</button>
+  )
+
   return (
     <main>
       <img src="../src/logo/2.png" className="yellow-img" alt="yellow logo" />
-      {questionData}
-      <button className="newquiz-btn">New Quiz</button>
+      {questionData}  
+      {totalLengthOfDataArray === sizeOfObject ? showAnswerButton : newQuizButton}
       <img src="../src/logo/1.png" className="blue-img" alt="blue logo" />
     </main>
   );
